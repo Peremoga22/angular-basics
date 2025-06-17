4393bd72a31a9c27fd622289fb22e1c8ae6f70fa
Vasyl Stolyarchuk <Vasyl.Stolyarchuk@outlook.com>
2025-06-17 21:05:15 +0300

    Adds post component to app component
    Adds the post component and displays it multiple times within the main application component.

---
+++ src/app/post/post.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({    
+    selector:'app-post',
+    standalone: true,
+    templateUrl:'./post.component.html'
+})
+
+export class PostComponent {}
